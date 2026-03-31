bc71dbd9413dbdb371c64b5171720ab5a0d3b4ee
Patrick Murray <113388917+mintbandit@users.noreply.github.com>
2026-03-31 16:08:07 +0100

    Added minimal storybook skeleton

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+
+
+/** @type { import('@storybook/web-components-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [],
+  "framework": "@storybook/web-components-vite"
+};
+export default config;
